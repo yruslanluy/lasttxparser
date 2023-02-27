05a20be70602733edef589c1beaad806020fce1b
Ruslan <48959405+yruslanluy@users.noreply.github.com>
2023-02-27 20:21:39 +0200

    Update appsScript.js

--- appsScript.js
+++ appsScript.js
@@ -6,17 +6,24 @@ const scan = ["https://api.arbiscan.io/api?module=account&action=txlist&address=
 function CallAPI(){
   const sheet = SpreadsheetApp.getActive().getSheetByName("Plan");
   for(let i = 0; i < wallets.length; i++){
-    for(let j = 0; j < scan.length; j++){
-      var response = UrlFetchApp.fetch(scan[j] + wallets[i] + "&startblock=1&endblock=99999999&sort=desc&apikey=YourApiKeyToken");
-      var data = JSON.parse(response.getContentText());
+    for(let j = 0; j < 3; j++){
       var cell = sheet.getRange(14 + i, 11 + j)
       try{
+        var response = UrlFetchApp.fetch(scan[j] + wallets[i] + "&startblock=1&endblock=99999999&sort=desc&apikey=YourApiKeyToken");
+        var data = JSON.parse(response.getContentText());
         cell.setValue(new Date(parseInt(data["result"][0]["timeStamp"]*1000)));
       }catch (error){
-        cell.setValue("Error");
+        wait(500);
+        try{
+          var response = UrlFetchApp.fetch(scan[j] + wallets[i] + "&startblock=1&endblock=99999999&sort=desc&apikey=YourApiKeyToken");
+          var data = JSON.parse(response.getContentText());
+          cell.setValue(new Date(parseInt(data["result"][0]["timeStamp"]*1000)));
+        }catch(error2){
+          cell.setValue("Error");
+        }
       }
     }
-    wait(1000);
+    wait(250);
   }
 }
 
